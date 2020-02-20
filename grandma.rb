15be04e2ac521479d835ef99b07b0e2e2a460612
Sean Reaves <seanitts@gmail.com>
2020-02-20 01:04:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,11 +11,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 require 'pry'
 def speak_to_grandma(phrase)
-  phrase
 binding.pry
 if phrase == '(I LOVE YOU GRANDMA!)'
   return '(I LOVE YOU TOO PUMPKIN!)'
-elsif phrase == phrase.uppercase
+elsif phrase == phrase.upcase
  return NO, NOT SINCE 1938!
 else
     'HUH?! SPEAK UP, SONNY!'
